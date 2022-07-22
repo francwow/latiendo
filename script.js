745b9306e7699be0d3c5cbc85dc4026d8f97ea4a
francwow <francwow06@gmail.com>
2022-07-22 13:07:03 -0500

    Add home carousel

--- script.js
+++ script.js
@@ -39,4 +39,100 @@ const navSlide = () => {
   });
 }
 
+
+const carousel = () => {
+  const carouselItem = document.querySelectorAll('.carousel_item');
+  console.log(carouselItem);
+
+  const dots = document.querySelector('.dots');
+  console.log(dots);
+
+  const dotsChildren = dots.children;
+  console.log(dotsChildren);
+
+  const dotOne = document.querySelector('.dot-1');
+  console.log(dotOne);
+
+  const dotTwo = document.querySelector('.dot-2');
+  console.log(dotTwo);
+
+  const dotThree = document.querySelector('.dot-3');
+  console.log(dotThree);
+
+  // Set Interval for Animation
+  let interval = setInterval(function() {
+    startCarousel();
+  }, 3000);
+
+  // Index for carouselItem Array
+  let index = 0;
+
+  // Function for adding and removing show class
+  startCarousel = () => {
+    index += 1;
+    if (index > carouselItem.length - 1) index = 0;
+
+    carouselItem.forEach((item, itemIndex) => {
+      if (itemIndex !== index) {
+        item.classList.remove('show');
+      } else {
+        item.classList.add('show');
+      }
+    }); 
+
+    for (i = 0; i < dotsChildren.length; i++) {
+      if (i !== index) {
+        dotsChildren[i].classList.remove('isGrey');
+      } else {
+        dotsChildren[i].classList.add('isGrey');
+      }
+    }
+  }
+
+  dotOne.addEventListener('click', () => {
+    clearInterval(interval);
+    index = 0;
+    carouselItem[0].classList.add('show');
+    carouselItem[1].classList.remove('show');
+    carouselItem[2].classList.remove('show');
+    dotOne.classList.add('isGrey');
+    dotTwo.classList.remove('isGrey');
+    dotThree.classList.remove('isGrey');
+    interval = setInterval(function() {
+      startCarousel();
+    }, 3000);
+  })
+
+  dotTwo.addEventListener('click', () => {
+    clearInterval(interval);
+    index = 1;
+    carouselItem[0].classList.remove('show');
+    carouselItem[1].classList.add('show');
+    carouselItem[2].classList.remove('show');
+    dotOne.classList.remove('isGrey');
+    dotTwo.classList.add('isGrey');
+    dotThree.classList.remove('isGrey');
+    interval = setInterval(function() {
+      startCarousel();
+    }, 3000);
+  })
+
+  dotThree.addEventListener('click', () => {
+    clearInterval(interval);
+    index = 2;
+    carouselItem[0].classList.remove('show');
+    carouselItem[1].classList.remove('show');
+    carouselItem[2].classList.add('show');
+    dotOne.classList.remove('isGrey');
+    dotTwo.classList.remove('isGrey');
+    dotThree.classList.add('isGrey');
+    interval = setInterval(function() {
+      startCarousel();
+    }, 3000);
+  })
+
+  // CONTRUIR FOR LOOP EVENTLISTENER PARA DOTS
+}
+
+carousel();
 navSlide();
